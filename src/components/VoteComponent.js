b324917c62d14b044925bf997cd75cde6f073160
Ajay Marathe <ma143ajay@gmail.com>
2021-09-07 15:10:14 +0530

    VoteComponent - added grid and devided screen into 2 etc other minor changes.

--- src/components/VoteComponent.js
+++ src/components/VoteComponent.js
@@ -1,12 +1,33 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, {useState} from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+import {Container, Grid, Row} from 'native-base';
 
 const VoteComponent = () => {
+  const [isVoteSubmitted, setIsVoteSubmitted] = useState(false);
   return (
-    <View>
-      <Text>VoteComponent</Text>
-    </View>
+    <Container>
+      <Grid>
+        <Row size={40}>
+          <Text
+            style={[
+              styles.pollHeading,
+              {color: isVoteSubmitted ? 'white' : '#0F0F0F'},
+            ]}>
+            Do you think this is good design for poll?
+          </Text>
+        </Row>
+        <Row size={60} />
+      </Grid>
+    </Container>
   );
 };
 
 export default VoteComponent;
+
+const styles = StyleSheet.create({
+  pollHeading: {
+    fontSize: 36,
+    width: '95%',
+    marginTop: 20,
+  },
+});
