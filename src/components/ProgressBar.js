235d5daae59ec692732b24c4ab7ed69fde9dc632
Ajay Marathe <ma143ajay@gmail.com>
2021-09-07 15:26:26 +0530

    ProgressBar - created ProgressBar Component.

---
+++ src/components/ProgressBar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const ProgressBar = () => {
+  return (
+    <View>
+      <Text>Progressbar</Text>
+    </View>
+  );
+};
+
+export default ProgressBar;
