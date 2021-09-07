70e3cbe6fd8530390da1ded2ebf58cbc85f1321b
Ajay Marathe <ma143ajay@gmail.com>
2021-09-07 15:16:41 +0530

    VoteComponent - updated second row content.

--- src/components/VoteComponent.js
+++ src/components/VoteComponent.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
 import {Container, Grid, Row} from 'native-base';
 
 const VoteComponent = () => {
@@ -7,7 +7,7 @@ const VoteComponent = () => {
   return (
     <Container>
       <Grid>
-        <Row size={40}>
+        <Row size={40} style={styles.firstRow}>
           <Text
             style={[
               styles.pollHeading,
@@ -16,7 +16,40 @@ const VoteComponent = () => {
             Do you think this is good design for poll?
           </Text>
         </Row>
-        <Row size={60} />
+        <Row size={60}>
+          <Grid>
+            <Row size={20} style={styles.progressRow}>
+              <TouchableOpacity disabled={isVoteSubmitted}>
+                <Text>ProgressBar</Text>
+              </TouchableOpacity>
+            </Row>
+            <Row size={20} style={styles.progressRow}>
+              <TouchableOpacity disabled={isVoteSubmitted}>
+                <Text>ProgressBar</Text>
+              </TouchableOpacity>
+            </Row>
+            <Row size={20} style={styles.progressRow}>
+              <TouchableOpacity disabled={isVoteSubmitted}>
+                <Text>ProgressBar</Text>
+              </TouchableOpacity>
+            </Row>
+            <Row size={20} style={{marginTop: 20, backgroundColor: '#45D3A5'}}>
+              <TouchableOpacity
+                style={styles.submitButton}
+                onPress={() => {
+                  setIsVoteSubmitted(!isVoteSubmitted);
+                }}>
+                {isVoteSubmitted ? (
+                  <Text style={styles.submitText}>
+                    Thank you for your response!
+                  </Text>
+                ) : (
+                  <Text style={styles.submitText}>Submit Vote!</Text>
+                )}
+              </TouchableOpacity>
+            </Row>
+          </Grid>
+        </Row>
       </Grid>
     </Container>
   );
@@ -29,5 +62,23 @@ const styles = StyleSheet.create({
     fontSize: 36,
     width: '95%',
     marginTop: 20,
+  },
+  firstRow: {
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  submitButton: {
+    width: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  submitText: {
+    color: 'white',
+    fontSize: 24,
+  },
+  progressRow: {
+    margin: 10,
+    justifyContent: 'center',
+    paddingHorizontal: 20,
   },
 });
