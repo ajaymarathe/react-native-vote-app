d848a7deb20f98d87779595a18e5071109d8b0a4
Ajay Marathe <ma143ajay@gmail.com>
2021-09-07 15:02:32 +0530

    VoteComponent - created vote component.

---
+++ src/components/VoteComponent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const VoteComponent = () => {
+  return (
+    <View>
+      <Text>VoteComponent</Text>
+    </View>
+  );
+};
+
+export default VoteComponent;
